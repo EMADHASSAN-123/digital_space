847026b0818890b39cebf6dc846401001ee60cce
EMADHASSAN-123 <emadaali62@gmail.com>
2025-10-03 00:35:44 +0300

    update_linl

--- admin/assets/js/config/appConfig.js
+++ admin/assets/js/config/appConfig.js
@@ -1,15 +1,15 @@
 export const APP_CONFIG = {
-  INDEX_PAGE:'/index.html',
+  INDEX_PAGE:'https://emadhassan-123.github.io/digital_space/index.html',
   POSTS_TABLE: 'posts',
   PAGE_SIZE: 12,
-  TINYMCE_PATH: '/assets/vendor/tinymce/tinymce.min.js',
-  POST_PAGE: '/public/page/post.html',   // صفحة عرض البوست
+  TINYMCE_PATH: 'https://emadhassan-123.github.io/digital_space/assets/vendor/tinymce/tinymce.min.js',
+  POST_PAGE: 'https://emadhassan-123.github.io/digital_space/public/page/post.html',   // صفحة عرض البوست
   RESOURCES_PAGE: '/public/page/resources.html', //  صفحة للمكتبة
-  ADMIN_PAGE: '/admin/index.html',
-  PROFILE_PAGE:'/public/page/profile.html',
-  INDEXSection_PAGE:'/index.html#categories-bar',
-  SINGUp_PAGE:'/public/page/signup.html',
-  LOGIN_PAGE:'/public/page/login.html',
-  post_detail:"/public/assets/js/post-detail.js",
-  header:"/public/assets/js/header.js",
+  ADMIN_PAGE: 'https://emadhassan-123.github.io/digital_space/admin/index.html',
+  PROFILE_PAGE:'https://emadhassan-123.github.io/digital_space/public/page/profile.html',
+  INDEXSection_PAGE:'https://emadhassan-123.github.io/digital_space/index.html#categories-bar',
+  SINGUp_PAGE:'https://emadhassan-123.github.io/digital_space/public/page/signup.html',
+  LOGIN_PAGE:'https://emadhassan-123.github.io/digital_space/public/page/login.html',
+  post_detail:"https://emadhassan-123.github.io/digital_space/public/assets/js/post-detail.js",
+  header:"https://emadhassan-123.github.io/digital_space/public/assets/js/header.js",
 };

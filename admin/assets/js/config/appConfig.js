8bbed7c29ab55c30036321a06eac29631935c97b
EMADHASSAN-123 <emadaali62@gmail.com>
2025-10-03 21:50:08 +0300

    update_link3

--- admin/assets/js/config/appConfig.js
+++ admin/assets/js/config/appConfig.js
@@ -1,16 +1,16 @@
 export const APP_CONFIG = {
-  INDEX_PAGE:'https://emadhassan-123.github.io/digital_space/index.html',
+  INDEX_PAGE:'      https://emadhassan-123.github.io/digital_space/index.html',
   POSTS_TABLE: 'posts',
   PAGE_SIZE: 12,
-  TINYMCE_PATH: 'https://emadhassan-123.github.io/digital_space/assets/vendor/tinymce/tinymce.min.js',
-  POST_PAGE: 'https://emadhassan-123.github.io/digital_space/public/page/post.html',   // صفحة عرض البوست
-  RESOURCES_PAGE: 'https://emadhassan-123.github.io/digital_space/public/page/resources.html', //  صفحة للمكتبة
-  ADMIN_PAGE: 'https://emadhassan-123.github.io/digital_space/admin/index.html',
-  PROFILE_PAGE:'https://emadhassan-123.github.io/digital_space/public/page/profile.html',
+  TINYMCE_PATH:     'https://emadhassan-123.github.io/digital_space/assets/vendor/tinymce/tinymce.min.js',
+  POST_PAGE:        'https://emadhassan-123.github.io/digital_space/public/page/post.html',   // صفحة عرض البوست
+  RESOURCES_PAGE:   'https://emadhassan-123.github.io/digital_space/public/page/resources.html', //  صفحة للمكتبة
+  ADMIN_PAGE:       'https://emadhassan-123.github.io/digital_space/admin/index.html',
+  PROFILE_PAGE:     'https://emadhassan-123.github.io/digital_space/public/page/profile.html',
   INDEXSection_PAGE:'https://emadhassan-123.github.io/digital_space/index.html#categories-bar',
-  SINGUp_PAGE:'https://emadhassan-123.github.io/digital_space/public/page/signup.html',
-  LOGIN_PAGE:'https://emadhassan-123.github.io/digital_space/public/page/login.html',
-  post_detail:"https://emadhassan-123.github.io/digital_space/public/assets/js/post-detail.js",
-  header:"https://emadhassan-123.github.io/digital_space/public/assets/js/header.js",
-  POST_EDITOR : "https://emadhassan-123.github.io/digital_space/admin/assets/js/features/post-editor.js",
+  SINGUp_PAGE:      'https://emadhassan-123.github.io/digital_space/public/page/signup.html',
+  LOGIN_PAGE:       'https://emadhassan-123.github.io/digital_space/public/page/login.html',
+  post_detail:      "https://emadhassan-123.github.io/digital_space/public/assets/js/post-detail.js",
+  header:           "https://emadhassan-123.github.io/digital_space/public/assets/js/header.js",
+  POST_EDITOR :     "https://emadhassan-123.github.io/digital_space/admin/assets/js/features/post-editor.js",
 };

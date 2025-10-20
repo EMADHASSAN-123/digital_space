fb0646c8a66c25c773d69d049f17623be097bbc2
EMADHASSAN-123 <emadaali62@gmail.com>
2025-10-21 01:33:01 +0300

    improve_links

--- admin/assets/js/config/appConfig.js
+++ admin/assets/js/config/appConfig.js
@@ -15,18 +15,18 @@
 //   POST_EDITOR :     "https://emadhassan-123.github.io/digital_space/admin/assets/js/features/post-editor.js",
 // }; 
 export const APP_CONFIG = {
-  INDEX_PAGE:'      https://emadhassan-123.github.io/digital_space/index.html',
+  INDEX_PAGE:'      https://digspace.netlify.app/',
   POSTS_TABLE: 'posts',
   PAGE_SIZE: 12, 
   TINYMCE_PATH:     'https://emadhassan-123.github.io/digital_space/assets/vendor/tinymce/tinymce.min.js',
-  POST_PAGE:        'https://emadhassan-123.github.io/digital_space/public/page/post.html',   // صفحة عرض البوست
-  RESOURCES_PAGE:   'https://emadhassan-123.github.io/digital_space/public/page/resources.html', //  صفحة للمكتبة
-  ADMIN_PAGE:       'https://emadhassan-123.github.io/digital_space/admin/index.html',
-  PROFILE_PAGE:     'https://emadhassan-123.github.io/digital_space/public/page/profile.html',
-  INDEXSection_PAGE:'https://emadhassan-123.github.io/digital_space/index.html#categories-bar',
-  SINGUp_PAGE:      'https://emadhassan-123.github.io/digital_space/public/page/signup.html',
-  LOGIN_PAGE:       'https://emadhassan-123.github.io/digital_space/public/page/login.html',
-  post_detail:      "https://emadhassan-123.github.io/digital_space/public/assets/js/post-detail.js",
-  header:           "https://emadhassan-123.github.io/digital_space/public/assets/js/header.js",
-  POST_EDITOR :     "https://emadhassan-123.github.io/digital_space/admin/post-editor.html",
+  POST_PAGE:        'https://digspace.netlify.app/public/page/post.html',   // صفحة عرض البوست
+  RESOURCES_PAGE:   'https://digspace.netlify.app/public/page/resources.html', //  صفحة للمكتبة
+  ADMIN_PAGE:       'https://digspace.netlify.app/admin/index.html',
+  PROFILE_PAGE:     'https://digspace.netlify.app/public/page/profile.html',
+  INDEXSection_PAGE:'https://digspace.netlify.app/index.html#categories-bar',
+  SINGUp_PAGE:      'https://digspace.netlify.app/public/page/signup.html',
+  LOGIN_PAGE:       'https://digspace.netlify.app/public/page/login.html',
+  post_detail:      "https://digspace.netlify.app/public/assets/js/post-detail.js",
+  header:           "https://digspace.netlify.app/public/assets/js/header.js",
+  POST_EDITOR :     "https://digspace.netlify.app/admin/post-editor.html",
 };
